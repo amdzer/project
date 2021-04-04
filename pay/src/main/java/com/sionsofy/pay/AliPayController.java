7fbe2562fd7e2f9f24d304bfac1cdf9a27c22573
wangweiwei <2062765610@qq.com>
2021-04-04 21:52:21 +0800

    aLiPayController

---
+++ pay/src/main/java/com/sionsofy/pay/AliPayController.java
@@ -0,0 +1,16 @@
+package com.sionsofy.pay;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: AliPayController
+ * @Description:
+ * @Author: wangweiwei
+ * @Date: 2021/4/4 9:40 PM
+ */
+@SpringBootApplication
+public class AliPayController {
+    public static void main(String[] args) {
+
+    }
+}
